5726e115eca3cbbd741fffffbfeb2f75f63171a9
fachry fathurahman <fachry123fathurahman@gmail.com>
2019-11-21 23:03:00 +0700

    homeActivity+navdraw 1.0

---
+++ app/src/main/java/com/infotech/riset/vodea/Home/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.infotech.riset.vodea.Home.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
