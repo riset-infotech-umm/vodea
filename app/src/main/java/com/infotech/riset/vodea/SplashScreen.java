a9d67b734499c5c650f67378a60da9c328cb344d
Augie Winanda Favourite <fifoaugie06@gmail.com>
2019-11-22 12:27:25 +0700

    add SplashScreen v1.0

--- app/src/main/java/com/infotech/riset/vodea/SplashScreen.java
+++ app/src/main/java/com/infotech/riset/vodea/SplashScreen.java
@@ -2,7 +2,12 @@ package com.infotech.riset.vodea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.infotech.riset.vodea.Home.HomeActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
     }
 }
