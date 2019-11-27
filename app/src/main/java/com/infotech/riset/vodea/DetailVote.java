db2cc6d4ff774f7425078ed89b64386d271c7f66
YusrilHasanuddin <48062400+YusrilHasanuddin@users.noreply.github.com>
2019-11-27 14:32:00 +0700

    commit y1

---
+++ app/src/main/java/com/infotech/riset/vodea/DetailVote.java
@@ -0,0 +1,14 @@
+package com.infotech.riset.vodea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailVote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_vote);
+    }
+}
